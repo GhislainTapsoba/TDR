fa9c3fbd0966d26d217facabedfc338274e19fae
GhislainTapsoba <ghislain443480@gmail.com>
2026-01-12 13:57:30 +0000

    résolution du problème d'auth

--- web-frontend/next.config.ts
+++ web-frontend/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // IMPORTANT pour Docker
-
+  // Désactiver Vercel Analytics
   experimental: {
     webVitalsAttribution: ['CLS', 'LCP']
   },
