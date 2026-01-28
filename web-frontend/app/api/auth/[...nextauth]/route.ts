1f07efcdc2d7e8d9559f523a64a30aced2408e3d
GhislainTapsoba <ghislain443480@gmail.com>
2026-01-28 19:54:10 +0000

    Ajout du nom de domaine

--- web-frontend/app/api/auth/[...nextauth]/route.ts
+++ web-frontend/app/api/auth/[...nextauth]/route.ts
@@ -18,11 +18,14 @@ const authOptions: AuthOptions = {
         }
 
         try {
-          const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(credentials),
-          });
+          const res = await fetch(
+            `${process.env.INTERNAL_API_URL}/login`,
+            {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(credentials),
+            }
+          );
 
           if (!res.ok) return null;
 
