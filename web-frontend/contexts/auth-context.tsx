23036a81fe16927d2fb707e7782019993f614509
GhislainTapsoba <ghislain443480@gmail.com>
2026-01-29 15:40:44 +0000

    Ajout du nom de domaine

--- web-frontend/contexts/auth-context.tsx
+++ web-frontend/contexts/auth-context.tsx
@@ -94,7 +94,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   if (rejectTask === 'true' && taskId) {
     router.push(`/reject-task?taskId=${taskId}`);
   } else if (urlParams.get('verify_task') === 'true' && taskId) {
-    router.push(`/dashboard/tasks/${taskId}`);
+    router.push(`/tasks/${taskId}`);
   } else {
     router.push('/dashboard');
   }
